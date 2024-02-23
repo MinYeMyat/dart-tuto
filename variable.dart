7613f2294a213b48b1043e23f0681a33dc42ddb7
MinYeMyat <130954290+MinYeMyat@users.noreply.github.com>
2024-02-23 23:44:53 +0900

    Initial commit

---
+++ variable.dart
@@ -0,0 +1,25 @@
+void main(){
+  int amount1 = 100;
+int amount2 = 200;
+
+print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+double dAmount1 = 100.11;
+var dAmount2 = 200.22;
+
+print('dAmount1: $dAmount1 | dAmount2: $dAmount2 \n');
+
+String name1 = 'MgYe';
+var name2 = 'YeYe';
+
+bool isItTrue1 = true;
+var isItTrue2 = false;
+
+print( 'isItTruel: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+ 
+dynamic weakVaribale = 100;
+print('WeakVariable 1 : $weakVaribale \n');
+
+weakVaribale = 'Dart Programming';
+print('WeakVariable 2: $weakVaribale');
+}
